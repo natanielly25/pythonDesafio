0c3c1904fcef797402f7e25c913f2edb57291e1c
natanielly <nataniellyr2@gmail.com>
2022-03-29 16:03:37 -0300

    Initial Commit

---
+++ desafio90.py
@@ -0,0 +1,13 @@
+aluno = dict()
+aluno ['Nome'] = str(input('Nome:'))
+aluno ['Média'] = float(input(f'Média de {aluno["Nome"] } '))
+if aluno['Média']>= 7:
+    aluno['Situação'] = 'Aprovado'
+elif 5 <= aluno ['Média'] < 7:
+    aluno['Situação'] = 'Reprovado'
+else:
+    aluno['Situação'] = 'Reprovado'
+print('=-=' * 20)
+for k, v in aluno.items():
+    print(f'  - {k} é igual a {v}')
+
